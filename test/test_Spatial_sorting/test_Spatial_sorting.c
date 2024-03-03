f5e881431a709baa515ed6dcefcb8b142d9cbbc2
RafaelVanaliDev <rafaelvanali@gmail.com>
2024-03-03 14:51:10 -0300

    Update test_Spatial_sorting.c
    Atualizando o bbox

--- test/test_Spatial_sorting/test_Spatial_sorting.c
+++ test/test_Spatial_sorting/test_Spatial_sorting.c
@@ -320,7 +320,26 @@ void __get_bounding_box(bbox_t* bbox, vertex_t** vertices_p, uint32_t npts)
     bbox->max[2] = (*vertices_p)[0].coord[2];
 
     for (uint32_t i = 1; i < npts; i++) {
-        // calcular xmin, xmax, ymin, ymax, zmin, zmax aqui
+        // Update bbbx to X
+        if ((*vertices_p)[i].coord[0] < bbox->min[0])
+            bbox->min[0] = (*vertices_p)[i].coord[0];
+
+        if ((*vertices_p)[i].coord[0] > bbox->max[0])
+            bbox->max[0] = (*vertices_p)[i].coord[0];
+
+        // Update bbbx to Y
+        if ((*vertices_p)[i].coord[1] < bbox->min[1])
+            bbox->min[1] = (*vertices_p)[i].coord[1];
+
+        if ((*vertices_p)[i].coord[1] > bbox->max[1])
+            bbox->max[1] = (*vertices_p)[i].coord[1];
+
+        // Update bbbx to Z
+        if ((*vertices_p)[i].coord[2] < bbox->min[2])
+            bbox->min[2] = (*vertices_p)[i].coord[2];
+
+        if ((*vertices_p)[i].coord[2] > bbox->max[2])
+            bbox->max[2] = (*vertices_p)[i].coord[2];
     }
 }
 
