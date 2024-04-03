0c68d648e4640249cb8c03fa7136c63893260f27
Vicente Sobrinho <vicente-sobrinho@courant.ufca.edu.br>
2024-04-03 10:48:39 -0300

    Fixed run.sh typo.

--- test/test_Spatial_sorting/run.sh
+++ test/test_Spatial_sorting/run.sh
@@ -1,4 +1,24 @@
 #!/bin/bash
+# Copyright (C) 2018 Célestin Marot
+#
+# This file is part of Spatial-kd-tree-sorting.
+#
+# This is a free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This software is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with hxt_SeqDel.  If not, see <http://www.gnu.org/licenses/>.
+#
+# See the COPYING file for the GNU General Public License.
+#
+# Author: Vicente Sobrinho (vicente.sobrinho@ufca.edu.br)
 
 runs="1 2 3"
 sizes="1 10 20 30 35 40"
@@ -14,7 +34,7 @@ for run in ${runs}; do
       echo "  Método: "${method}
       for data in ${datasets}; do
         echo "    | Processando o conjunto '"${data}"' com "${size}"M pontos"
-        ./bin/Release/test_Spatial_sorting --${method} --${data}000000 ${size} >> ${method}-${data}${size}M.txt 2>&1
+        ./bin/Release/test_Spatial_sorting --${method} --${data} ${size}000000 >> ${method}-${data}${size}M.txt 2>&1
       done
     done
   done
@@ -26,7 +46,7 @@ for method in ${methods}; do
   mkdir -p ${folder}
   mv ${method}-*.txt ${folder}/
   zip ${folder}.zip ${folder}/*
-  
+
   echo "  > Enviando os resultados do método " ${method}
   echo "That's all folks!" | mutt -s "${folder}" -a "${folder}".zip -- ${to}
 done
